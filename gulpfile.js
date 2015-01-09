4b4d083ad75a82c238b05827841c295c7e0dc6d5
Eric Barbour <emb4gu@virginia.edu>
2015-01-09 00:09:25 -0500

    Version 2.0.0. New file structure, lots of fun gulp tasks and bower components

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,62 @@
 /*!
  * gulp
- * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ * $ npm install gulp-ruby-sass gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
  */
 
 // Load plugins
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del');
+sass = require('gulp-ruby-sass'),
+minifycss = require('gulp-minify-css'),
+jshint = require('gulp-jshint'),
+uglify = require('gulp-uglify'),
+rename = require('gulp-rename'),
+concat = require('gulp-concat'),
+notify = require('gulp-notify'),
+cache = require('gulp-cache'),
+livereload = require('gulp-livereload'),
+del = require('del');
 
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('assets/css/main.sass')
-    .pipe(sass({ style: 'expanded', }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+  return gulp.src('app/css/main.sass')
+  .pipe(sass({ style: 'expanded', }))
+  .pipe(gulp.dest('dist/css'))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(minifycss())
+  .pipe(gulp.dest('dist/css'))
+  .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('assets/js/*.js')
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+  return gulp.src('app/js/*.js')
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
+  .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-// Clean
+// Clean and rebuild dist folders
 gulp.task('clean', function(cb) {
-    del(['dist/assets/css', 'dist/assets/js'], cb)
-});
+  del(['dist/css', 'dist/js'], cb)
+})
 
 // Default task
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts');
+  gulp.start('styles', 'scripts');
 });
 
 // Watch
 gulp.task('watch', function() {
 
   // Watch .scss files
-  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch('app/css/**/*.sass', ['styles']);
 
   // Watch .js files
-  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch('app/js/**/*.js', ['scripts']);
 
   // Create LiveReload server
   livereload.listen();
